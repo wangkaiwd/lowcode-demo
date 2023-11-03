43758e0e8783481c96f72afc77d304d4aaab628a
wangkaiwd <1162486176@qq.com>
2023-11-03 17:03:37 +0800

    fix: handle some detail problem

--- src/core/MarkerLines/index.tsx
+++ src/core/MarkerLines/index.tsx
@@ -1,40 +1,41 @@
-import css from './index.module.less';
-import { useEditorStore } from '@/store/editStore.ts';
-import { getSelectedComponent } from '@/store/helper.ts';
-import { useMemo } from 'react';
+import css from './index.module.less'
+import { useEditorStore } from '@/store/editStore.ts'
+import { getSelectedComponent, updateSelectedComponents } from '@/store/helper.ts'
+import { useMemo } from 'react'
 
-const diff = 4;
+const diff = 4
 const MarkerLines = () => {
-  const storeState = useEditorStore();
-  const { components } = storeState;
-  const selectedComponent = getSelectedComponent(storeState);
+  const storeState = useEditorStore()
+  const { components } = storeState
+  const selectedComponent = getSelectedComponent(storeState)
   const linesCoordinate = useMemo(() => {
     if (!selectedComponent) {
-      return null;
+      return null
     }
     for (let i = 0; i < components.length; i++) {
-      const component = components[i];
+      const component = components[i]
       if (component.uid === selectedComponent.uid) {
-        continue;
+        continue
       }
-      const { left: cmpLeft }: any = component.wrapperStyle;
-      const { left: selectedCmpLeft }: any = selectedComponent.wrapperStyle;
+      const { left: cmpLeft }: any = component.wrapperStyle
+      const { left: selectedCmpLeft }: any = selectedComponent.wrapperStyle
       if (Math.abs(cmpLeft - selectedCmpLeft) <= diff) {
+        updateSelectedComponents({ wrapperStyle: { left: cmpLeft } })
         return {
-          left: cmpLeft
-        };
+          left: cmpLeft - 1
+        }
       }
     }
-  }, [components, selectedComponent]);
+  }, [components, selectedComponent])
   return (
     <div className={css.markerLines}>
       {
         linesCoordinate?.left &&
         <div className={css.lineY} style={{ left: linesCoordinate.left }}></div>
       }
-      <div className={css.lineX}></div>
+      {/*<div className={css.lineX}></div>*/}
     </div>
-  );
-};
+  )
+}
 
-export default MarkerLines;
+export default MarkerLines
--- src/hooks/useMove.tsx
+++ src/hooks/useMove.tsx
@@ -1,8 +1,8 @@
-import { useMemoizedFn } from './useMemoizedFn.ts';
-import { throttle } from 'lodash-es';
-import React, { useRef } from 'react';
+import { useMemoizedFn } from './useMemoizedFn.ts'
+import { throttle } from 'lodash-es'
+import React, { useRef } from 'react'
 
-import { updateSelectedComponents } from '../store/helper.ts';
+import { updateSelectedComponents } from '../store/helper.ts'
 
 interface StartCoordinate {
   startX: number;
@@ -15,52 +15,52 @@ interface UseMoveOptions {
 }
 
 export const useMove = (options: UseMoveOptions = {}) => {
-  const { zoom = 1 } = options;
-  const startCoordinate = useRef<StartCoordinate | null>(null);
+  const { zoom = 1 } = options
+  const startCoordinate = useRef<StartCoordinate | null>(null)
   const onMove = useMemoizedFn(throttle((e: MouseEvent) => {
     if (!startCoordinate.current) {
-      return;
+      return
     }
-    const { startX, startY } = startCoordinate.current;
-    const { clientX, clientY } = e;
-    const distanceX = (clientX - startX) / zoom;
-    const distanceY = (clientY - startY) / zoom;
+    const { startX, startY } = startCoordinate.current
+    const { clientX, clientY } = e
+    const distanceX = (clientX - startX) / zoom
+    const distanceY = (clientY - startY) / zoom
     updateSelectedComponents((preProps) => {
-      if (!preProps.wrapperStyle) {return {};}
-      const { left, top } = preProps.wrapperStyle;
+      if (!preProps.wrapperStyle) {return {}}
+      const { left, top } = preProps.wrapperStyle
       return {
         wrapperStyle: {
           left: left as number + distanceX,
           top: top as number + distanceY
         }
-      };
-    });
+      }
+    })
     startCoordinate.current = {
       startX: clientX,
       startY: clientY
-    };
-  }, 50));
+    }
+  }))
 
   const onMouseDown = (e: React.MouseEvent) => {
-    const { clientX: startX, clientY: startY } = e;
+    const { clientX: startX, clientY: startY } = e
     startCoordinate.current = {
       startX,
       startY
-    };
-    options.onMouseDown?.(e);
+    }
+    options.onMouseDown?.(e)
     // avoid image move affect
     // e.preventDefault();
     const onUp = () => {
-      startCoordinate.current = null;
-      document.removeEventListener('mousemove', onMove);
-      document.removeEventListener('mouseup', onUp);
-      document.removeEventListener('mouseleave', onUp);
-    };
-    document.addEventListener('mousemove', onMove);
-    document.addEventListener('mouseup', onUp);
-    document.addEventListener('mouseleave', onUp);
-  };
+      startCoordinate.current = null
+      document.removeEventListener('mousemove', onMove)
+      document.removeEventListener('mouseup', onUp)
+      document.removeEventListener('mouseleave', onUp)
+    }
+    document.addEventListener('mousemove', onMove)
+    document.addEventListener('mouseup', onUp)
+    document.addEventListener('mouseleave', onUp)
+  }
   return {
     onMouseDown
-  };
-};
+  }
+}
